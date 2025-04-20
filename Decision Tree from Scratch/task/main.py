a4fcb609535c7f0fd73c03ab3873c300c5611afa
Trung Dung Nguyen <dung.nguyen@code4you.com>
2025-04-20 13:53:28 +0200

    update

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -1,24 +1,35 @@
-# write your code here
-node = [int(x) for x in input().split(" ")]
+# # write your code here
+# node = [int(x) for x in input().split(" ")]
+#
+# class_0 = 0
+# class_1 = 1
+#
+# def calculate_probability(class_i, arr):
+#     counter = arr.count(class_i)
+#     return counter / len(arr)
+#
+# def calculate_gini(arr):
+#     p_0 = calculate_probability(class_0, arr)
+#     p_1 = calculate_probability(class_1, arr)
+#     return 1 - p_0 ** 2 - p_1 ** 2
+#
+# split_1 = [int(x) for x in input().split(" ")]
+# split_2 = [int(x) for x in input().split(" ")]
+#
+# def calculate_weighted_gini():
+#     gini_index_1 = calculate_gini(split_1)
+#     gini_index_2 = calculate_gini(split_2)
+#     return round(len(split_1) / len(node) * gini_index_1 + len(split_2) / len(node) * gini_index_2, 5)
+#
+# print(f"{round(calculate_gini(node), 2)} {round(calculate_weighted_gini(), 2)}")
 
-class_0 = 0
-class_1 = 1
 
-def calculate_probability(class_i, arr):
-    counter = arr.count(class_i)
-    return counter / len(arr)
+def subtract(a, b):
+    return a - b
 
-def calculate_gini(arr):
-    p_0 = calculate_probability(class_0, arr)
-    p_1 = calculate_probability(class_1, arr)
-    return 1 - p_0 ** 2 - p_1 ** 2
-
-split_1 = [int(x) for x in input().split(" ")]
-split_2 = [int(x) for x in input().split(" ")]
-
-def calculate_weighted_gini():
-    gini_index_1 = calculate_gini(split_1)
-    gini_index_2 = calculate_gini(split_2)
-    return round(len(split_1) / len(node) * gini_index_1 + len(split_2) / len(node) * gini_index_2, 5)
-
-print(f"{round(calculate_gini(node), 2)} {round(calculate_weighted_gini(), 2)}")
+# subtract(a=3, 1)
+# print(subtract(3, b=1))
+# print(subtract(3, 1))
+# print(subtract(b=3, a=1))
+print(subtract(3, a=1))
+# subtract(a=3, b=1)
